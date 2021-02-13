edc79c66f69846319f6fbfa711e24673395bd262
funduval <funduval@gmail.com>
2021-02-12 22:37:14 -0500

    testing add

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash";
-import Pagination from "./pagination";
 
 const Pagination = (props) => {
   const { itemsCount, pageSize, currentPage, onPageChange } = props;
